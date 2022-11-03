79dee22f18ad098e3d3b6698530ce6b30f910cc3
serajang99 <serajang1679@gmail.com>
2022-11-03 15:17:34 +0900

    feat: kakao 지도 api 연결

--- src/main/java/com/staya/asap/Controller/ParkingController.java
+++ src/main/java/com/staya/asap/Controller/ParkingController.java
@@ -2,9 +2,18 @@ package com.staya.asap.Controller;
 
 import com.staya.asap.Model.DB.ParkingDTO;
 import com.staya.asap.Service.ParkingService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Map;
 
 @RestController //@Controller + @ResponseBody
 @RequestMapping("/api/parking")
@@ -32,9 +41,25 @@ public class ParkingController {
         return "parkinglot save done";
     }
 
-    @GetMapping("/data")
-    public String showParkingLotData()
-    {
-        return "parkinglot data";
+    @GetMapping("/latlng")
+    public Map getLatLng(@RequestParam String searching) {
+//        String searching = "신촌역 주차장";
+        String url = "https://dapi.kakao.com/v2/local/search/keyword.json?query="+searching+"&category_group_code=PK6&radius=20000";
+        String key = "d08482d7a9775511b47dfeaa8b8997f7";
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization", "KakaoAK " + key); //Authorization 설정
+        HttpEntity<String> httpEntity = new HttpEntity<>(httpHeaders); //엔티티로 만들기
+        URI targetUrl = UriComponentsBuilder
+                .fromUriString(url) //기본 url
+                .queryParam("query", searching) //인자
+                .build()
+                .encode(StandardCharsets.UTF_8) //인코딩
+                .toUri();
+
+        //GetForObject는 헤더를 정의할 수 없음
+        ResponseEntity<Map> result = restTemplate.exchange(targetUrl, HttpMethod.GET, httpEntity, Map.class);
+        return result.getBody(); //내용 반환
     }
 }
