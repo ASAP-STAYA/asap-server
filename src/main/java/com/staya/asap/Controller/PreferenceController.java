97fda37bba4adce1123c6252c1520689ad9124a2
sen <seeunchoi99@gmail.com>
2022-11-18 23:50:20 +0900

    feat: add PreferenceController

---
+++ src/main/java/com/staya/asap/Controller/PreferenceController.java
@@ -0,0 +1,25 @@
+package com.staya.asap.Controller;
+
+import com.staya.asap.Model.DB.PreferenceDTO;
+import com.staya.asap.Service.PreferenceService;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/preference")
+@CrossOrigin(maxAge = 3600)
+public class PreferenceController {
+    private PreferenceService preferenceService;
+
+    public PreferenceController(PreferenceService preferenceService) {
+        this.preferenceService = preferenceService;
+    }
+
+    @GetMapping("/{userId}")
+    public PreferenceDTO getPreferenceInfoFromUserId(@PathVariable("userId") Integer id) {
+        return preferenceService.getPreferenceByUserId(id);
+    }
+}
