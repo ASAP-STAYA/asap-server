595401b7c092364d66ac1e9a457d81600159f3f8
Jenny-Oh <ojy1369@naver.com>
2022-11-24 01:53:19 +0900

    review updated FIN

--- src/main/java/com/staya/asap/Controller/ReviewController.java
+++ src/main/java/com/staya/asap/Controller/ReviewController.java
@@ -2,8 +2,13 @@ package com.staya.asap.Controller;
 
 
 import com.staya.asap.Configuration.Security.Auth.PrincipalDetails;
+import com.staya.asap.Model.DB.PreferenceDTO;
 import com.staya.asap.Model.DB.ReviewDTO;
+import com.staya.asap.Service.PreferenceService;
 import com.staya.asap.Service.ReviewService;
+import org.apache.commons.collections4.sequence.EditScript;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,16 +18,51 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
 
     private ReviewService reviewService;
-
+    @Autowired
+    private PreferenceService preferenceService;
 
     public ReviewController(ReviewService reviewService) {this.reviewService = reviewService;}
+
+    // 리뷰로 상대 중요도 업데이트
+    public Boolean updateWeights(ReviewDTO review){
+        Boolean updated = false;
+        PreferenceDTO prefer = preferenceService.getPreferenceByUserId(review.getUser_id());
+
+        // 어쩔 수 없이 배정된 기준 밖의 주차장의 경우 => 중요도 업데이트 필요 X
+        if (review.getDist() > prefer.getDist_prefer() || review.getCost() > prefer.getCost_prefer()){
+            return updated;
+        }
+
+        System.out.println(prefer.getDist_weight());
+        System.out.println(prefer.getCost_weight());
+        // 불만족 항목에 대한 상대 중요도를 높인다
+        // discontent : 거리 불만족 = 0 / 요금 불만족 = 1
+        if (review.getDiscontent() == 0){
+            // 거리 불만족  == 거리가 중요한 사람 -> 요금 중요도 * 0.8
+            double weight = prefer.getCost_weight()*0.8;
+            prefer.setCost_weight(weight);
+            prefer.setDist_weight(1-weight);
+        } else {
+            double weight = prefer.getDist_weight()*0.8;
+            prefer.setDist_weight(weight);
+            prefer.setCost_weight(1-weight);
+        }
+        System.out.println(prefer.getDist_weight());
+        System.out.println(prefer.getCost_weight());
+        preferenceService.updateWeights(prefer, review.getUser_id());
+        return updated;
+    }
 
     @PostMapping("/save")
     public String saveReview(@RequestBody ReviewDTO review){
         PrincipalDetails user = (PrincipalDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         review.setUser_id(user.getUserId());
+        review.setId(0);
         reviewService.saveReview(review);
 
+        if (review.getDiscontent() >= 0){
+            updateWeights(review);
+        }
         return "Review save done";
     }
 
