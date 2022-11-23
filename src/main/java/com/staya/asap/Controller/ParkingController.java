a3b811697cbe4c5f00539b4e1d308729c4fb4417
serajang99 <serajang1679@gmail.com>
2022-11-23 14:37:02 +0900

    feat: file upload api

--- src/main/java/com/staya/asap/Controller/ParkingController.java
+++ src/main/java/com/staya/asap/Controller/ParkingController.java
@@ -4,15 +4,26 @@ import com.staya.asap.ExcelToDatabase;
 import com.staya.asap.Model.DB.ParkingDTO;
 import com.staya.asap.Service.ParkingService;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.openxml4j.opc.OPCPackage;
+import org.apache.poi.openxml4j.util.ZipSecureFile;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
@@ -35,11 +46,58 @@ public class ParkingController {
 
 
     @GetMapping("/save")
-    public String saveParkingLotData() throws IOException, InvalidFormatException {
-        ExcelToDatabase excelToDatabase = new ExcelToDatabase();
+    public String saveParkingLotData(@RequestParam(value = "fileUpload") MultipartFile mf) throws Exception {
 
-        List<ParkingDTO> parkinglots = excelToDatabase.upload();
-        System.out.println("save parkinglots::"+parkinglots);
+        ArrayList<ParkingDTO> parkinglots = new ArrayList<>();
+
+        OPCPackage opcPackage = OPCPackage.open(mf.getInputStream());
+        XSSFWorkbook workbook = new XSSFWorkbook(opcPackage);
+
+        XSSFSheet sheet = workbook.getSheetAt(0); //행의 수
+        int rows = sheet.getPhysicalNumberOfRows();
+        Sheet worksheet = workbook.getSheetAt(0);
+
+        for (int rowindex = 1; rowindex < rows; rowindex++) { //행을읽는다
+            Row row = worksheet.getRow(rowindex);
+
+            ParkingDTO parkingDTO = new ParkingDTO();
+
+            parkingDTO.setPARKING_CODE((int) row.getCell(0).getNumericCellValue());
+            parkingDTO.setPARKING_NAME(row.getCell(1).getStringCellValue());
+            parkingDTO.setADDR(row.getCell(2).getStringCellValue());
+            parkingDTO.setPARKING_TYPE(row.getCell(3).getStringCellValue());
+            parkingDTO.setTEL(row.getCell(4).getStringCellValue());
+
+            parkingDTO.setCAPACITY((int) row.getCell(5).getNumericCellValue());
+            parkingDTO.setCAPACITY_AVAILABLE((int) row.getCell(6).getNumericCellValue());
+
+            parkingDTO.setWEEKDAY_BEGIN_TIME((int) row.getCell(7).getNumericCellValue());
+            parkingDTO.setWEEKDAY_END_TIME((int) row.getCell(8).getNumericCellValue());
+            parkingDTO.setWEEKEND_BEGIN_TIME((int) row.getCell(9).getNumericCellValue());
+            parkingDTO.setWEEKEND_END_TIME((int) row.getCell(10).getNumericCellValue());
+            parkingDTO.setHOLIDAY_BEGIN_TIME((int) row.getCell(11).getNumericCellValue());
+            parkingDTO.setHOLIDAY_END_TIME((int) row.getCell(12).getNumericCellValue());
+
+            parkingDTO.setPAY_YN(row.getCell(13).getBooleanCellValue());
+            parkingDTO.setSATURDAY_PAY_YN(row.getCell(14).getBooleanCellValue());
+            parkingDTO.setHOLIDAY_PAY_YN(row.getCell(15).getBooleanCellValue());
+
+            parkingDTO.setRATES((int) row.getCell(16).getNumericCellValue());
+            parkingDTO.setTIME_RATE((int) row.getCell(17).getNumericCellValue());
+            parkingDTO.setADD_RATES((int) row.getCell(18).getNumericCellValue());
+            parkingDTO.setADD_TIME_RATE((int) row.getCell(19).getNumericCellValue());
+
+            parkingDTO.setDAY_MAXIMUM((int) row.getCell(20).getNumericCellValue());
+            parkingDTO.setLAT(row.getCell(21).getNumericCellValue());
+            parkingDTO.setLNG(row.getCell(22).getNumericCellValue());
+
+            parkingDTO.setWIDE_YN(row.getCell(23).getBooleanCellValue());
+            parkingDTO.setMECHANICAL_YN(row.getCell(24).getBooleanCellValue());
+            parkingDTO.setRATES_PER_HOUR((int) row.getCell(25).getNumericCellValue());
+
+//            System.out.println("parkingDTO : "+parkingDTO);
+            parkinglots.add(parkingDTO);
+        }
 
         for (ParkingDTO parkingDTO : parkinglots) {
             parkingService.saveParkingLot(parkingDTO);
