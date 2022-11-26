2b02268268c4fa46dd46bd11c36c80f831e8670b
serajang99 <serajang1679@gmail.com>
2022-11-26 22:28:13 +0900

    fix: review 적용

--- src/main/java/com/staya/asap/Model/DB/PreferenceDTO.java
+++ src/main/java/com/staya/asap/Model/DB/PreferenceDTO.java
@@ -8,29 +8,17 @@ import lombok.Setter;
 @Getter
 @Setter
 @Data
-@Entity
-@Table(name = "preference")
 public class PreferenceDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    @Column
     private int user_id;
-
-    @Column
     private Double dist_prefer;
-    @Column
     private Double dist_weight;
-    @Column
     private Double cost_prefer;
-    @Column
     private Double cost_weight;
 
-    @Column
     private Boolean can_mechanical;
-    @Column
     private Boolean can_narrow;
 
 }
